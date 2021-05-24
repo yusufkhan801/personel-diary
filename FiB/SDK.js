e5d1c6b45daf878dc0d0a786bd3e44634ce9bbd1
yusuf khan <yusufkhanmd.1234@gmail.com>
2021-05-24 09:17:39 +0530

    created project may 24,2021
    added firebase SDK
    created main html and css files

---
+++ FiB/SDK.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCyN2dMR-7T0xr_glBv0jGugEPV1m3VkLo",
+    authDomain: "diaryme-cb0ba.firebaseapp.com",
+    projectId: "diaryme-cb0ba",
+    storageBucket: "diaryme-cb0ba.appspot.com",
+    messagingSenderId: "913501035874",
+    appId: "1:913501035874:web:1ce0266cc08fd2b673e03b",
+    measurementId: "G-VLMH5JDSSR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
